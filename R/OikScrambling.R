0fbc6f2bc55d94b5b5cda13f2c7a8edd86d5426c
Charles Plessy <charles.plessy@oist.jp>
2021-09-08 22:20:42 +0000

    Placeholders

---
+++ R/OikScrambling.R
@@ -0,0 +1,10 @@
+#' The OikScrambling package
+#'
+#' The purpose of this package is to version-control and manage a bunch of
+#' vignettes.  For this reason it does not provide functions.
+#'
+#' @author The Luscombe laboratory
+#'
+#' @docType package
+#' @name OikScrambling-package
+NULL
