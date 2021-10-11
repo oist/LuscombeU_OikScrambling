e8084fc6d4d09b7a8e91b5f8df123942fc6cddc2
Charles Plessy <charles.plessy@oist.jp>
2021-10-11 05:07:55 +0000

    Accessory function to load all repeats.

---
+++ R/loadAllRepeats.R
@@ -0,0 +1,42 @@
+#' Load all repeat annotations
+#'
+#' Load all the repeat annotations distributed by the `BreakpointsData` data
+#' package, and sort them in broad classes by parsing their `Target` attribute.
+#'
+#' @returns Returns a `SimpleList` of all loaded repeat annotations.
+
+loadAllRepeats <- function() {
+  reps <- SimpleList()
+  reps$Oki <- rtracklayer::import(system.file("extdata/Annotations/OKI2018_I69.v2/OKI2018_I69.repeats.gff", package = "BreakpointsData"))
+  reps$Osa <- rtracklayer::import(system.file("extdata/Annotations/OSKA2016v1.9/OSKA2016v1.9.repeats.gff",  package = "BreakpointsData"))
+  reps$Bar <- rtracklayer::import(system.file("extdata/Annotations/Bar2_p4.Flye/Bar2_p4.Flye.repeats.gff",  package = "BreakpointsData"))
+  reps$Kum <- rtracklayer::import(system.file("extdata/Annotations/KUM-M3-7f/KUM-M3-7f.repeats.gff",        package = "BreakpointsData"))
+  reps$Aom <- rtracklayer::import(system.file("extdata/Annotations/AOM-5-5f/AOM-5-5f.repeats.gff",          package = "BreakpointsData"))
+  reps <- endoapply(reps, \(x) {
+    x$Class <- x$Target                                              |>
+      sub(pat = '".*',                     rep = "")                 |>
+      sub(pat = "Motif:",                  rep = "")                 |>
+      sub(pat = "\\(.+)n",                 rep = "tandem")           |>
+      sub(pat = "_family.*",               rep = "")                 |>
+      sub(pat = "SINE.*",                  rep = "SINE")             |>
+      sub(pat = "i69_juicer.*",            rep = "unknown")          |>
+      sub(pat = "oska2016v1.9_.*",         rep = "unknown")          |>
+      sub(pat = "bar2_p4_.*",              rep = "unknown")          |>
+      sub(pat = "kum-m3-7f_.*",            rep = "unknown")          |>
+      sub(pat = "aom-5-5f_.*",             rep = "unknown")          |>
+      sub(pat = "Chr1",                    rep = "unknown")          |>
+      sub(pat = "Chr2",                    rep = "unknown")          |>
+      sub(pat = "PAR",                     rep = "unknown")          |>
+      sub(pat = "S107",                    rep = "unknown")          |>
+      sub(pat = "S144",                    rep = "unknown")          |>
+      sub(pat = "S215",                    rep = "unknown")          |>
+      sub(pat = "XSR",                     rep = "unknown")          |>
+      sub(pat = "contig.*",                rep = "unknown")          |>
+      sub(pat = "HiC_scaffold_.*",         rep = "unknown")          |>
+      sub(pat = ".*-rich",                 rep = "LowComplexity")    |>
+      sub(pat = "rnd.*",                   rep = "rnd")              |>
+      factor()
+    x
+  })
+  reps |> as("SimpleList")
+}
