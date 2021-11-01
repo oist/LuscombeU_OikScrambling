694f5a7370bb313244e883371c0283b65556aeea
Charles Plessy <charles.plessy@oist.jp>
2021-11-01 05:56:33 +0000

    Push compDistance function.

---
+++ R/compDistance.R
@@ -0,0 +1,19 @@
+#' Transform pair name into distance class.
+#'
+#' We have 3 main evolutionary distances: _same population_, _North Atlantic vs
+#' North Pacific_, and _Okinawa vs the other populations_.  To ease plotting
+#' this function transforms the names of pairwise comparisons in one of these
+#' three classes.
+#'
+#' @param x A vector of pair names such as `Oki_Kum`, `Osa_Nor`, etc
+#'
+#' @return A vector of "`same_pop"`, `"Oki – North"` and `"North – North"`.
+
+compDistance <- function(x) {
+  x[x %in% c("Oki_Kum", "Osa_Aom", "Bar_Nor")]                       <- "same_pop"
+  x[x %in% c("Oki_Osa", "Oki_Bar", "Oki_Kum", "Oki_Aom", "Oki_Nor",
+             "Osa_Oki", "Osa_Kum", "Bar_Oki", "Bar_Kum")]            <- "Oki – North"
+  x[x %in% c("Osa_Bar", "Osa_Aom", "Osa_Nor",
+             "Bar_Osa", "Bar_Aom", "Bar_Nor")]                       <- "North – North"
+  x
+}
