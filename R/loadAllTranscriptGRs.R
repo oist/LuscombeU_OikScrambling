defa96479c94f4acac3e3b70bb51ab2a1fcfa730
Charles Plessy <charles.plessy@oist.jp>
2021-11-10 07:54:14 +0000

    Load annotations, but as GRanges of transcripts.

---
+++ R/loadAllTranscriptGRs.R
@@ -0,0 +1,14 @@
+#' Load all transcript information as GRanges
+#'
+#' Accessory function to load all transcript annotations provided by the
+#' `BreakpointsData` package
+#'
+#' @returns Returns a [`SimpleList`] of `GRanges` objects representing the
+#' annotations of the genomes we study.
+
+loadAllTranscriptsGR <- function() {
+  genomes <- OikScrambling:::loadAllGenomes()
+  annots <- OikScrambling:::loadAllAnnotations() |> suppressWarnings()
+  transcripts <- sapply(annots, GenomicFeatures::transcripts) |> SimpleList()
+  transcripts
+}
