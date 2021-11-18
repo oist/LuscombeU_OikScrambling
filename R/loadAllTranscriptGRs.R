40d9e8a8ae43b47d76f7352c23c26f0231c531eb
Charles Plessy <charles.plessy@oist.jp>
2021-11-18 01:48:28 +0000

    Import dNdS values in `loadAllTranscriptsGR`

--- R/loadAllTranscriptGRs.R
+++ R/loadAllTranscriptGRs.R
@@ -8,9 +8,23 @@
 
 loadAllTranscriptsGR <- function() {
   genomes <- OikScrambling:::loadAllGenomes()
+  # Load TxDB annotations
   annots <- OikScrambling:::loadAllAnnotations() |> suppressWarnings()
+  # Convert to GRanges on transcript features.
   transcripts <- sapply(annots, GenomicFeatures::transcripts) |> SimpleList()
-  # Start modifying the object here.
+  # Load dNdS values
+  dnds <- SimpleList()
+  dnds$dnds_guidance   <- loaddNdSTable("v2.0.0/genewise_oikopleura.extended.txt", alignment_type = 'GUIDANCE2')
+  dnds$dnds_hmmcleaner <- loaddNdSTable("v2.0.0/genewise_oikopleura.extended.txt", alignment_type = 'HmmCleaner')
+  dnds$dnds_prank      <- loaddNdSTable("v2.0.0/genewise_oikopleura.extended.txt", alignment_type = 'Alignment')
+  # Stitch them in the objects
+  transcripts <- sapply(names(transcripts), function(sp){
+    sp_dnds = addMetadataToGRanges(gr=transcripts[[sp]], df=dnds$dnds_guidance[[sp]], gr_key='tx_name', df_key='transcript_id_simple', df_col_name = 'dNdS', rename_meta = 'dNdS_GUIDANCE2')
+    sp_dnds = addMetadataToGRanges(gr=sp_dnds, df=dnds$dnds_hmmcleaner[[sp]], gr_key='tx_name', df_key='transcript_id_simple', df_col_name = 'dNdS', rename_meta = 'dNdS_HmmCleaner')
+    sp_dnds = addMetadataToGRanges(gr=sp_dnds, df=dnds$dnds_prank[[sp]], gr_key='tx_name', df_key='transcript_id_simple', df_col_name = 'dNdS', rename_meta = 'dNdS_PRANK')
+    sp_dnds
+  })
+  # Give transcript names to the rows
   transcripts <- sapply(transcripts, \(x) {names(x) <- x$tx_name ; x}) |> SimpleList()
   transcripts
 }
