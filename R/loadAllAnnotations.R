d0ddd5827ec67db093a24bdf6f94af0cfc88bb8c
Charles Plessy <charles.plessy@oist.jp>
2021-11-19 06:02:08 +0000

    Load Ciona Plymouth and Roscoff annotations.
    (But dNdS values are not available).

--- R/loadAllAnnotations.R
+++ R/loadAllAnnotations.R
@@ -29,12 +29,21 @@ loadAllAnnotations <- function() {
     tx <- GenomicFeatures::makeTxDbFromGRanges(tx)
   }
 
+  gff2txdb_other <- function(file) {
+    file <- system.file(paste0("extdata/Annotations/", file), package = "BreakpointsData")
+    tx <- rtracklayer::import.gff(file)
+    tx <- GRanges(tx)
+    tx <- GenomicFeatures::makeTxDbFromGRanges(tx)
+  }
+
   annots$Oki <- gff2txdb("OKI2018_I69.v2/OKI2018_I69.v2.gm.gff.gz",  OKI2018_I69)
   annots$Osa <- gff2txdb("OSKA2016v1.9/OSKA2016v1.9.gm.gff.gz",      OSKA2016v1.9)
   annots$Bar <- gff2txdb("Bar2_p4.Flye/Bar2_p4.Flye.gm.gff.gz",      Bar2_p4)
   annots$Kum <- gff2txdb("KUM-M3-7f/KUM-M3-7f.gm.gff.gz",            KUM_M3)
   annots$Aom <- gff2txdb("AOM-5-5f/AOM-5-5f.gm.gff.gz",              AOM_5)
   annots$Nor <- gff2txdb_Norway("OdB3/Oikopleura_annot_v1.0.gff.gz", OdB3)
+  annots$Ply <- gff2txdb_other("C_int_P/C_int_P.gm.gff.gz")
+  annots$Ros <- gff2txdb_other("C_int_R/C_int_R.gm.gff.gz")
 
   annots
 }
